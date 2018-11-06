344bb470ffd76783716f1a90dee19f18266a2afe
SeonHyungJo <seonhyung.jo@gmail.com>
2018-11-07 00:02:30 +0900

    :apple: Create models and Modified files

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = ({ store }) => (
+    <div>
+        main
+        {/* <Header addTodo={store.addTodo} />
+        <MainSection store={store} /> */}
+    </div>
+)
+
+export default App
